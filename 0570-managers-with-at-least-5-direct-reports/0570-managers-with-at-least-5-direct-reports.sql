af91ff789f7cdbd4a332cf7b13826f4cd330c91e
Rohan Patil <72259041+RohanPatil01@users.noreply.github.com>
2024-02-24 22:13:02 +0530

    Time: 901 ms (16.13%), Space: 0B (100.00%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT name FROM Employee
+WHERE id in (SELECT managerId  FROM Employee
+            GROUP BY managerId
+            HAVING COUNT(managerId) >= 5)
